589b7c9fcdccbac25b95574113fd1d19b0c6d3ee
vaibhav-aiplanet <vaibhav@aiplanet.com>
2025-02-20 00:40:40 +0530

    VPC and Subnets added

--- main.tf
+++ main.tf
@@ -10,3 +10,40 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+data "aws_availability_zones" "available" {}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id           = aws_vpc.main.id
+  cidr_block       = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id           = aws_vpc.main.id
+  cidr_block       = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
