40b0645c05d9aa031329df1a2630ef8916961534
vaibhav-aiplanet <vaibhav@aiplanet.com>
2025-02-20 00:51:30 +0530

    Route Table Rules added

--- main.tf
+++ main.tf
@@ -47,3 +47,19 @@ resource "aws_subnet" "private" {
   cidr_block       = var.private_subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
