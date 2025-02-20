3547d8f9e529a2a503e2796ed1578cb07b6e0a42
vaibhav-aiplanet <vaibhav@aiplanet.com>
2025-02-20 11:48:51 +0530

    NAT gateway added for private subnets

--- network.tf
+++ network.tf
@@ -6,6 +6,16 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+    }
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+
 resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
   vpc_id           = aws_vpc.main.id
--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "private_subnet_ids" {
 output "instance_public_ip" {
   value = aws_instance.nginx.public_ip
 }
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
