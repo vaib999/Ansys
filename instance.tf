ff48cfe5e3423bb2e775f007623a116f50618f9a
vaibhav-aiplanet <vaibhav@aiplanet.com>
2025-02-21 01:24:07 +0530

    User data for nginx added

--- instance.tf
+++ instance.tf
@@ -4,4 +4,12 @@ resource "aws_instance" "nginx" {
   subnet_id              = aws_subnet.public[0].id
   security_groups        = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install nginx -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              EOF
 }
